4f73a5fbb2afd660b97c23c346d392c596a9c47d
sdotson <stu.dotson@gmail.com>
2021-01-30 09:26:57 -0500

    latest build

---
+++ src/keys.js
@@ -0,0 +1,8 @@
+export const firebaseKeys = {
+  apiKey: 'AIzaSyDsI-WePDEVH-ipRRYuV-uUZiLZBljN-dg',
+  authDomain: 'korn-fan-site.firebaseapp.com',
+  projectId: 'korn-fan-site',
+  storageBucket: 'korn-fan-site.appspot.com',
+  messagingSenderId: '283242489356',
+  appId: '1:283242489356:web:d3555a6276f86e495af029'
+};
